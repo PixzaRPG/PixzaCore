c7d994ab5b3c5d93ec650bff427f7ffcae96529c
WillQi <williamqipizza@gmail.com>
2021-06-08 02:20:25 -0600

    connect to redis

--- pixzabungee/src/main/java/io/github/pixzarpg/core/bungee/PixzaBungeeConfig.java
+++ pixzabungee/src/main/java/io/github/pixzarpg/core/bungee/PixzaBungeeConfig.java
@@ -15,4 +15,20 @@ public class PixzaBungeeConfig {
         return this.config.getFloat("server_target_capacity");
     }
 
+    public String getRedisHost() {
+        return this.config.getString("host");
+    }
+
+    public int getRedisPort() {
+        return this.config.getInt("port");
+    }
+
+    public String getRedisUsername() {
+        return this.config.getString("username");
+    }
+
+    public String getRedisPassword() {
+        return this.config.getString("password");
+    }
+
 }
--- pixzabungee/src/main/java/io/github/pixzarpg/core/bungee/load/LoadBalancer.java
+++ pixzabungee/src/main/java/io/github/pixzarpg/core/bungee/load/LoadBalancer.java
@@ -2,6 +2,7 @@ package io.github.pixzarpg.core.bungee.load;
 
 import io.github.pixzarpg.core.bungee.PixzaBungeeConfig;
 import net.md_5.bungee.api.config.ServerInfo;
+import redis.clients.jedis.JedisPool;
 
 import java.util.Collection;
 
@@ -10,10 +11,19 @@ public class LoadBalancer {
     private final PixzaBungeeConfig config;
     private final Collection<ServerInfo> servers;
 
+    private final JedisPool connectionPool;
+
 
     public LoadBalancer(PixzaBungeeConfig config, Collection<ServerInfo> servers) {
         this.config = config;
         this.servers = servers;
+
+        this.connectionPool = new JedisPool(
+                this.config.getRedisHost(),
+                this.config.getRedisPort(),
+                this.config.getRedisUsername(),
+                this.config.getRedisPassword());
+
     }
 
 }
