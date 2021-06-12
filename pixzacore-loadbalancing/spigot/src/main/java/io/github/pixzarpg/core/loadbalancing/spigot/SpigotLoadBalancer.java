ad8648733d8609744c04a9b8c978dc81d4740db8
WillQi <williamqipizza@gmail.com>
2021-06-12 04:00:06 -0600

    Empty spigot plugin for load balancing

---
+++ pixzacore-loadbalancing/spigot/src/main/java/io/github/pixzarpg/core/loadbalancing/spigot/SpigotLoadBalancer.java
@@ -0,0 +1,12 @@
+package io.github.pixzarpg.core.loadbalancing.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotLoadBalancer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+    
+}
