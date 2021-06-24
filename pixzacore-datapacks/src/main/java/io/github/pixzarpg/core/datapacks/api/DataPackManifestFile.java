a7d74b6ed544614118c6f53ce37b9fa00872600b
WillQi <williamqipizza@gmail.com>
2021-06-23 18:53:53 -0600

    manifest parser

--- pixzacore-datapacks/src/main/java/io/github/pixzarpg/core/datapacks/api/DataPackManifestFile.java
+++ pixzacore-datapacks/src/main/java/io/github/pixzarpg/core/datapacks/api/DataPackManifestFile.java
@@ -14,10 +14,73 @@ public class DataPackManifestFile {
     private Dependency[] dependencies = new Dependency[0];
 
 
+    public UUID getUuid() {
+        return this.uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public int getVersion() {
+        return this.version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Dependency[] getDependencies() {
+        return this.dependencies;
+    }
+
+    public void setDependencies(Dependency[] dependencies) {
+        this.dependencies = dependencies;
+    }
+
+
     public static class Dependency {
 
-        private UUID uuid;
-        private int version;
+        private final UUID uuid;
+        private final int version;
+
+
+        public Dependency(UUID uuid, int version) {
+            this.uuid = uuid;
+            this.version = version;
+        }
+
+        public UUID getUuid() {
+            return this.uuid;
+        }
+
+        public int getVersion() {
+            return this.version;
+        }
 
     }
 
--- pixzacore-datapacks/src/main/java/io/github/pixzarpg/core/datapacks/versions/v1/parsers/V1DataPackManifestFileParser.java
+++ pixzacore-datapacks/src/main/java/io/github/pixzarpg/core/datapacks/versions/v1/parsers/V1DataPackManifestFileParser.java
@@ -1,14 +1,41 @@
 package io.github.pixzarpg.core.datapacks.versions.v1.parsers;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import io.github.pixzarpg.core.datapacks.DataPackFileParser;
 import io.github.pixzarpg.core.datapacks.api.DataPackManifestFile;
+
+import java.util.UUID;
 
 public class V1DataPackManifestFileParser implements DataPackFileParser<DataPackManifestFile> {
 
     @Override
     public DataPackManifestFile parse(JsonObject data) {
-        return null;
+        DataPackManifestFile manifestFile = new DataPackManifestFile();
+
+        // Version of the parser to use
+        manifestFile.setVersion(data.get("version").getAsInt());
+
+        // General info
+        JsonObject manifestInfo = data.getAsJsonObject("info");
+        manifestFile.setName(manifestInfo.get("name").getAsString());
+        manifestFile.setDescription(manifestInfo.get("description").getAsString());
+        manifestFile.setAuthor(manifestInfo.get("author").getAsString());
+        manifestFile.setUuid(UUID.fromString(manifestInfo.get("uuid").getAsString()));
+
+        // dependencies
+        JsonArray jsonDependencies = data.get("dependencies").getAsJsonArray();
+        DataPackManifestFile.Dependency[] dependencies = new DataPackManifestFile.Dependency[jsonDependencies.size()];
+        for (int i = 0; i < jsonDependencies.size(); i++) {
+            JsonObject jsonDependency = jsonDependencies.get(i).getAsJsonObject();
+            dependencies[i] = new DataPackManifestFile.Dependency(
+                    UUID.fromString(jsonDependency.get("uuid").getAsString()),
+                    jsonDependency.get("version").getAsInt()
+            );
+        }
+        manifestFile.setDependencies(dependencies);
+
+        return manifestFile;
     }
 
 }
